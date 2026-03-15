d6be793d90c91c374da52687530d6bcfe01f7ad5
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2026-03-15 08:58:45 +0000

    restart

--- music_controller/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from .views import RoomView
-
-
-urlpatterns = [
-    path("room/", RoomView.as_view()),
-]
--- music_controller/frontend/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import index
-from django.urls import include, path
-
-urlpatterns = [
-
-    path('', index),
-]
