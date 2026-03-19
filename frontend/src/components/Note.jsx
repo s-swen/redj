85e7454730ac6d32c70e458efb0d4fe8d8036044
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2026-03-19 09:50:36 +0000

    note component

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,16 @@
+function Note({ note, onDelete }) {
+  return (
+    <div className="note-container">
+      <p className="note-title">{note.title}</p>
+      <p className="note-content">{note.content}</p>
+      <p className="note-date">
+        {new Date(note.created_at).toLocaleDateString("en-US")}
+      </p>
+      <button className="delete-button" onClick={() => onDelete(note.is)}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default Note;
