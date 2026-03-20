c57ca086a45020a04d0d55d26a2e5849b51b0bbc
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2026-03-20 09:42:19 +0000

    fixing err

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../api";
 import Note from "../components/Note";
+import "../styles/Home.css";
 
 function Home() {
   const [notes, setNotes] = useState([]);
